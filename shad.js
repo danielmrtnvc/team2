13839268ca0687d91ff92f7e8560e4a6f124edaa
Thomas Lin <37830237+Thomasqazwsxedc@users.noreply.github.com>
2019-07-17 15:52:17 -0400

    Update shad.js

--- shad.js
+++ shad.js
@@ -1,40 +1,24 @@
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(1400, 800, WEBGL);
 }
 
 function draw() {
-//  background(102);
-clear();
-  push();
-  translate(width * 0.2, height * 0.5);
-  rotate(frameCount / 200.0);
-  star(0, 0, 5, 70, 3);
-  pop();
+  clear();
+  rotateY(frameCount * 0.01);
 
-  push();
-  translate(width * 0.5, height * 0.5);
-  rotate(frameCount / 50.0);
-  star(0, 0, 80, 100, 40);
-  pop();
-
-  push();
-  translate(width * 0.8, height * 0.5);
-  rotate(frameCount / -100.0);
-  star(0, 0, 30, 70, 5);
-  pop();
-}
-
-function star(x, y, radius1, radius2, npoints) {
-  let angle = TWO_PI / npoints;
-  let halfAngle = angle / 2.0;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius2;
-    let sy = y + sin(a) * radius2;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius1;
-    sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
+  for (let j = 0; j < 5; j++) {
+    push();
+    for (let i = 0; i < 80; i++) {
+      translate(
+        sin(frameCount * 0.001 + j) * 100,
+        sin(frameCount * 0.001 + j) * 100,
+        i * 0.1
+      );
+      rotateZ(frameCount * 0.002);
+      push();
+      sphere(8, 6, 4);
+      pop();
+    }
+    pop();
   }
-  endShape(CLOSE);
 }
